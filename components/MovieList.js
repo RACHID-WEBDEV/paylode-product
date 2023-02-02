7e47495e1baba7937d2e47c6e2e9132212984b5b
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2023-02-02 23:20:16 +0100

    Remove old reducer file2

--- components/MovieList.js
+++ components/MovieList.js
@@ -40,7 +40,7 @@ const MovieList = () => {
                     </Link>
                     <p className="mb-3 font-normal text-gray-700 ">{item.description}</p>
                     <Link href={`/movie/${item.id}`}>
-                      <a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 ">
+                      <a className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-800 hover:text-white focus:ring-4 focus:outline-none focus:ring-gray-300 ">
                         Read more
                         <svg aria-hidden="true" className="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
                       </a>
