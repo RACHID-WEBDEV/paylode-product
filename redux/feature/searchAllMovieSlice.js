13df49054afd974e9f8b790468d076791cd5af36
RACHID-WEBDEV <rasheedolamide2015@gmail.com>
2023-02-02 21:42:16 +0100

    Remove old reducer file

--- redux/feature/searchAllMovieSlice.js
+++ redux/feature/searchAllMovieSlice.js
@@ -37,31 +37,3 @@ export default searchAllMovieSlice.reducer;
 
 
 // import { createSlice } from '@reduxjs/toolkit';
-
-// const searchAllMovieSlice = createSlice({
-//     name: 'searchMovie',
-//     initialState: {
-//         searchMoviesList: [],
-//         searchMovie: {}
-//     },
-//     reducers: {
-//         getSearchMovies(name) {
-//             return name;
-//         },
-//         setSearchMovies: (state, action) => {
-//             state.searchMoviesList = action.payload;
-//         },
-//         getSearchMovie(id) {
-//             return id;
-//         },
-//         setSearchMovie: (state, action) => {
-//             state.movie = action.payload;
-//         },
-
-//     }
-// });
-
-
-// export const { getSearchMovies, setSearchMovies, getSearchMovie, setSearchMovie } = searchAllMovieSlice.actions;
-
-// export default searchAllMovieSlice.reducer;
